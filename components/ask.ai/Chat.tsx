532eed69c40c6f480c7d30a81cdde87eddb660b6
Muhammad18557 <muhammad.abdullah@u.yale-nus.edu.sg>
2024-08-19 01:01:03 +0800

    types added

--- components/ask.ai/Chat.tsx
+++ components/ask.ai/Chat.tsx
@@ -3,8 +3,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./Chat.module.css";
 import { AssistantStream } from "openai/lib/AssistantStream";
+import { AssistantStreamEvent } from "openai/resources/beta/assistants/assistants";
 import Markdown from "react-markdown";
 import { RequiredActionFunctionToolCall } from "openai/resources/beta/threads/runs/runs";
+import { TextDelta } from "openai/resources/beta/threads/messages.mjs";
+import {
+  ToolCallDelta,
+  ToolCall,
+} from "openai/resources/beta/threads/runs/steps.mjs";
 
 const sampleQuestions = [
   "What is Abdullah's expected graduation date?",
@@ -66,7 +72,7 @@ const Chat = ({
   functionCallHandler = () => Promise.resolve(""), // default to return empty string
 }: ChatProps) => {
   const [userInput, setUserInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<MessageProps[]>([]);
   const [inputDisabled, setInputDisabled] = useState(false);
   const [threadId, setThreadId] = useState("");
 
@@ -91,7 +97,7 @@ const Chat = ({
     createThread();
   }, []);
 
-  const sendMessage = async (text) => {
+  const sendMessage = async (text: string) => {
     const response = await fetch(
       `/api/assistants/threads/${threadId}/messages`,
       {
@@ -101,11 +107,13 @@ const Chat = ({
         }),
       }
     );
-    const stream = AssistantStream.fromReadableStream(response.body);
+    const stream = response.body
+      ? AssistantStream.fromReadableStream(response.body)
+      : null;
     handleReadableStream(stream);
   };
 
-  const submitActionResult = async (runId, toolCallOutputs) => {
+  const submitActionResult = async (runId: any, toolCallOutputs: any) => {
     const response = await fetch(
       `/api/assistants/threads/${threadId}/actions`,
       {
@@ -119,11 +127,13 @@ const Chat = ({
         }),
       }
     );
-    const stream = AssistantStream.fromReadableStream(response.body);
+    const stream = AssistantStream.fromReadableStream(
+      response.body as ReadableStream<any>
+    );
     handleReadableStream(stream);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     if (!userInput.trim()) return;
     sendMessage(userInput);
@@ -144,7 +154,7 @@ const Chat = ({
   };
 
   // textDelta - append text to last assistant message
-  const handleTextDelta = (delta) => {
+  const handleTextDelta = (delta: TextDelta, snapshot: any) => {
     if (delta.value != null) {
       appendToLastMessage(delta.value);
     }
@@ -154,20 +164,20 @@ const Chat = ({
   };
 
   // imageFileDone - show image in chat
-  const handleImageFileDone = (image) => {
+  const handleImageFileDone = (image: { file_id: any }) => {
     appendToLastMessage(`\n![${image.file_id}](/api/files/${image.file_id})\n`);
   };
 
   // toolCallCreated - log new tool call
-  const toolCallCreated = (toolCall) => {
+  const toolCallCreated = (toolCall: { type: string }) => {
     if (toolCall.type != "code_interpreter") return;
     appendMessage("code", "");
   };
 
   // toolCallDelta - log delta and snapshot for the tool call
-  const toolCallDelta = (delta, snapshot) => {
+  const toolCallDelta = (delta: ToolCallDelta, snapshot: ToolCall) => {
     if (delta.type != "code_interpreter") return;
-    if (!delta.code_interpreter.input) return;
+    if (!delta.code_interpreter?.input) return;
     appendToLastMessage(delta.code_interpreter.input);
   };
 
@@ -179,7 +189,7 @@ const Chat = ({
     const toolCalls = event.data.required_action.submit_tool_outputs.tool_calls;
     // loop over tool calls and call function handler
     const toolCallOutputs = await Promise.all(
-      toolCalls.map(async (toolCall) => {
+      toolCalls.map(async (toolCall: RequiredActionFunctionToolCall) => {
         const result = await functionCallHandler(toolCall);
         return { output: result, tool_call_id: toolCall.id };
       })
@@ -219,7 +229,7 @@ const Chat = ({
     =======================
   */
 
-  const appendToLastMessage = (text) => {
+  const appendToLastMessage = (text: string) => {
     setMessages((prevMessages) => {
       const lastMessage = prevMessages[prevMessages.length - 1];
       const updatedLastMessage = {
@@ -230,24 +240,30 @@ const Chat = ({
     });
   };
 
-  const appendMessage = (role, text) => {
+  const appendMessage = (role: "user" | "assistant" | "code", text: string) => {
     setMessages((prevMessages) => [...prevMessages, { role, text }]);
   };
 
-  const annotateLastMessage = (annotations) => {
+  const annotateLastMessage = (annotations: any[]) => {
     setMessages((prevMessages) => {
       const lastMessage = prevMessages[prevMessages.length - 1];
       const updatedLastMessage = {
         ...lastMessage,
       };
-      annotations.forEach((annotation) => {
-        if (annotation.type === "file_path") {
-          updatedLastMessage.text = updatedLastMessage.text.replaceAll(
-            annotation.text,
-            `/api/files/${annotation.file_path.file_id}`
-          );
+      annotations.forEach(
+        (annotation: {
+          type: string;
+          text: string | RegExp;
+          file_path: { file_id: any };
+        }) => {
+          if (annotation.type === "file_path") {
+            updatedLastMessage.text = updatedLastMessage.text.replaceAll(
+              annotation.text,
+              `/api/files/${annotation.file_path.file_id}`
+            );
+          }
         }
-      });
+      );
       return [...prevMessages.slice(0, -1), updatedLastMessage];
     });
   };
